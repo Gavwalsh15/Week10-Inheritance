a5afea89f8a3abf57c69d0f0e65707b0fd930752
Gavin Walsh <G00402296@atu.ie>
2022-11-28 09:37:48 +0000

    Added added getters setters and customer class

--- src/main/java/ie/atu/week10/Inheritance.java
+++ src/main/java/ie/atu/week10/Inheritance.java
@@ -1,4 +1,14 @@
 package ie.atu.week10;
 
+import java.util.Scanner;
+
 public class Inheritance {
+    public static void main(String[] args) {
+       Person gavin = new Person();
+       gavin.setName("Gavin");
+       gavin.setAddress("H91YXC4");
+       gavin.setTelephone("085 7486559");
+
+        System.out.println("My details are\n" + gavin.getName() +"\n"+ gavin.getAddress()+"\n" + gavin.getTelephone());
+    }
 }
