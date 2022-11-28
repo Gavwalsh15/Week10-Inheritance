4e0f7204634103165b3a1ada77f1e70527ba7361
Gavin Walsh <G00402296@atu.ie>
2022-11-28 10:32:21 +0000

    Added to string

--- src/main/java/ie/atu/week10/Inheritance.java
+++ src/main/java/ie/atu/week10/Inheritance.java
@@ -3,13 +3,14 @@ package ie.atu.week10;
 import java.util.Scanner;
 
 public class Inheritance {
+
     public static void main(String[] args) {
        Person gavin = new Person();
        gavin.setName("Gavin");
        gavin.setAddress("H91YXC4");
        gavin.setTelephone("085 7486559");
 
-        System.out.println("My details are:\n" + gavin.getName() +"\n"+ gavin.getAddress()+"\n" + gavin.getTelephone());
+        System.out.println("My details are:\n" + gavin.toString());
 
         Customer cillian = new Customer();
         cillian.setName("Cillian");
@@ -18,7 +19,9 @@ public class Inheritance {
         cillian.setCustomerNumber("420");
         cillian.setMailingList(true);
 
-        System.out.println("Cillian's details are:\n" + cillian.getName() +"\n"+ cillian.getAddress()+"\n" + cillian.getTelephone()
-                +"\n"+ cillian.getCustomerNumber()+"\n"+ cillian.isMailingList());
+
+
+        System.out.println("Cillian's details are:\n" + cillian.toString());
+
     }
 }
